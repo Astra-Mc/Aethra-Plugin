ec6029d7cad26b84fb7f5ec47699a7abbc3efaf4
Adrian <adrian.olbrich1@gmail.com>
2023-11-11 22:23:36 +0100

    added rank support to PlayerDB

--- src/main/java/astra/mongodb/PlayerDB.java
+++ src/main/java/astra/mongodb/PlayerDB.java
@@ -5,6 +5,8 @@ import astra.lang.LangConfig;
 import astra.mongodb.codec.quests.PlayerQuestCodec;
 import astra.plugin;
 import astra.playerquestsystem.PlayerQuest;
+import astra.ranks.RankConfig;
+import astra.sidepanel.SidePanel;
 import cn.nukkit.Player;
 import cn.nukkit.Server;
 import com.mongodb.client.model.Filters;
@@ -20,7 +22,7 @@ public class PlayerDB {
     public static void Start() {
     }
 
-    public static void addPlayerToDB(String xuid, UUID uuid, String name, LangConfig.Languages language) {
+    public static void addPlayer(String xuid, UUID uuid, String name, LangConfig.Languages language) {
 
         Document playerDocument = new Document()
                 .append("xuid", xuid)
@@ -28,6 +30,8 @@ public class PlayerDB {
                 .append("name", name)
                 .append("lang", language)
                 .append("coins", 0L)
+                .append("ranks", List.of(RankConfig.Ranks.MEMBER))
+                .append("selected_rank", RankConfig.Ranks.MEMBER)
                 .append("skill_points", 0)
                 .append("skills", new HashMap<String, Integer>())
                 .append("quests", List.of());
@@ -37,7 +41,7 @@ public class PlayerDB {
         plugin.getInstance().getLogger().info(String.format(
                 "Added Player to PlayerDB\nXUID: %s\nUUID: %s\nNAME: %s\nLANG: %s",
                 xuid,
-                uuid.toString(),
+                uuid,
                 name,
                 language
         ));
@@ -50,7 +54,7 @@ public class PlayerDB {
 
     // GETTERS
 
-    public static UUID getPlayerUUIDFromDB(Player player) {
+    public static UUID getPlayerUUID(Player player) {
         Document result = MongoDB.getPlayerCollection().find(Filters.eq("xuid", player.getLoginChainData().getXUID())).first();
         if (result != null) {
             return UUID.fromString(result.get("uuid", String.class));
@@ -58,7 +62,7 @@ public class PlayerDB {
             return null;
         }
     }
-    public static String getPlayNameFromDB(Player player) {
+    public static String getPlayName(Player player) {
         Document result = MongoDB.getPlayerCollection().find(Filters.eq("xuid", player.getLoginChainData().getXUID())).first();
         if (result != null) {
             return result.get("name", String.class);
@@ -66,15 +70,15 @@ public class PlayerDB {
             return null;
         }
     }
-    public static LangConfig.Languages getPlayerLanguageFromDB(Player player) {
+    public static LangConfig.Languages getPlayerLanguage(Player player) {
         Document result = MongoDB.getPlayerCollection().find(Filters.eq("xuid", player.getLoginChainData().getXUID())).first();
         if (result != null) {
-            return result.get("lang", LangConfig.Languages.class);
+            return LangConfig.Languages.valueOf(result.get("lang", String.class));
         } else {
             return null;
         }
     }
-    public static long getPlayerCoinsFromDB(Player player) {
+    public static long getPlayerCoins(Player player) {
         Document result = MongoDB.getPlayerCollection().find(Filters.eq("xuid", player.getLoginChainData().getXUID())).first();
         if (result != null) {
             return (result.get("coins", Long.class));
@@ -113,30 +117,54 @@ public class PlayerDB {
             return null;
         }
     }
+    public static List<RankConfig.Ranks> getPlayerRanks(Player player) {
+        Document result = MongoDB.getPlayerCollection().find(Filters.eq("xuid", player.getLoginChainData().getXUID())).first();
+        if (result != null) {
+            List<String> data = result.get("ranks", List.class);
+            List<RankConfig.Ranks> returnData = List.of();
+            for (String rank: data) {
+                returnData.add(RankConfig.Ranks.valueOf(rank));
+            }
+            return returnData;
+        } else {
+            return null;
+        }
+    }
+    public static RankConfig.Ranks getSelectedPlayerRank(Player player) {
+        Document result = MongoDB.getPlayerCollection().find(Filters.eq("xuid", player.getLoginChainData().getXUID())).first();
+        if (result != null) {
+            return RankConfig.Ranks.valueOf(result.get("selected_rank", String.class));
+        } else {
+            return null;
+        }
+    }
 
     // SETTERS
 
-    public static void setPlayerUUIDInDB(Player player, UUID uuid) {
+    public static void setPlayerUUID(Player player, UUID uuid) {
         Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
         Bson update = Updates.set("uuid", uuid.toString());
         MongoDB.getPlayerCollection().updateOne(filter, update);
     }
-    public static void setPLayerNameInDB(Player player, String name) {
+    public static void setPLayerName(Player player, String name) {
         Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
         Bson update = Updates.set("name", name);
         MongoDB.getPlayerCollection().updateOne(filter, update);
+
+        SidePanel.sendPlayerName(player);
     }
-    public static void setPlayerLanguageInDB(Player player, LangConfig.Languages language) {
+    public static void setPlayerLanguage(Player player, LangConfig.Languages language) {
         Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
         Bson update = Updates.set("lang", language);
         MongoDB.getPlayerCollection().updateOne(filter, update);
     }
-    public static void setPlayerCoinsInDB(Player player, long coins) {
+    public static void setPlayerCoins(Player player, long coins) {
         Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
         Bson update = Updates.set("coins", coins);
         MongoDB.getPlayerCollection().updateOne(filter, update);
 
-        Server.getInstance().getPluginManager().callEvent(new CoinsUpdateEvent(player, PlayerDB.getPlayerCoinsFromDB(player)));
+        Server.getInstance().getPluginManager().callEvent(new CoinsUpdateEvent(player, PlayerDB.getPlayerCoins(player)));
+        SidePanel.sendPlayerCoins(player);
     }
     public static void setPlayerSkillPoints(Player player, int points) {
         Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
@@ -148,32 +176,43 @@ public class PlayerDB {
         Bson update = Updates.set("skill_points", skills);
         MongoDB.getPlayerCollection().updateOne(filter, update);
     }
-    public static void setPlayerQuests(Player player, List<PlayerQuest> playerQuests){
+    public static void setAllPlayerQuests(Player player, List<PlayerQuest> playerQuests){
         Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
         Bson update = Updates.set("quests", playerQuests);
         MongoDB.getPlayerCollection().updateOne(filter, update);
     }
-
-    public static void setPlayerQuest(Player player, PlayerQuest playerQuest){
+    public static void setOnePlayerQuest(Player player, PlayerQuest playerQuest){
         List<PlayerQuest> playerQuests = getPlayerQuests(player);
         for (PlayerQuest quest : playerQuests) {
             if (Objects.equals(quest.getID(), playerQuest.getID())){
                 playerQuests.remove(quest);
                 playerQuests.add(playerQuest);
-                PlayerDB.setPlayerQuests(player, playerQuests);
+                PlayerDB.setAllPlayerQuests(player, playerQuests);
                 break;
             }
         }
     }
+    public static void setPlayerRanks(Player player, List<RankConfig.Ranks> ranks) {
+        Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
+        Bson update = Updates.set("ranks", ranks);
+        MongoDB.getPlayerCollection().updateOne(filter, update);
+    }
+    public static void setSelectedPlayerRank(Player player, RankConfig.Ranks rank){
+        Bson filter = Filters.eq("xuid", player.getLoginChainData().getXUID());
+        Bson update = Updates.set("rank", rank);
+        MongoDB.getPlayerCollection().updateOne(filter, update);
+
+        SidePanel.sendPlayerSelectedRank(player);
+    }
+
 
     // ADDERS & REMOVERS
 
     public static void addPlayerQuest(Player player, PlayerQuest playerQuest){
         List<PlayerQuest> playerQuests = getPlayerQuests(player);
         playerQuests.add(playerQuest);
-        setPlayerQuests(player, playerQuests);
+        setAllPlayerQuests(player, playerQuests);
     }
-
     public static void removePlayerQuest(Player player, PlayerQuest playerQuest){
         List<PlayerQuest> playerQuests = getPlayerQuests(player);
         for (PlayerQuest quest : playerQuests) {
@@ -182,6 +221,16 @@ public class PlayerDB {
                 break;
             }
         }
-        setPlayerQuests(player, playerQuests);
+        setAllPlayerQuests(player, playerQuests);
+    }
+    public static void addPlayerRank(Player player, RankConfig.Ranks rank) {
+        List<RankConfig.Ranks> ranks = getPlayerRanks(player);
+        ranks.add(rank);
+        setPlayerRanks(player, ranks);
+    }
+    public static void removePlayerRank(Player player, RankConfig.Ranks rank) {
+        List<RankConfig.Ranks> ranks = getPlayerRanks(player);
+        ranks.remove(rank);
+        setPlayerRanks(player, ranks);
     }
 }
